fcd55c2fa383a52fc57b4ab25a266356e80cab75
JulioC090 <juliocziviani@gmail.com>
2024-08-11 18:02:31 -0300

    feat: add loadConfig function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Command } from 'commander';
+import fs from 'node:fs';
 
 const program = new Command();
+
+function loadConfig(path: string): unknown {
+  const data = fs.existsSync(path) ? fs.readFileSync(path, 'utf-8') : '{}';
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    console.error('Failed to parse JSON:', e);
+    return {};
+  }
+}
+
+const config = loadConfig('./config.json');
+console.log(config);
 
 program.command('save [name] [origin]').action((name, origin) => {
   console.log(`Saving ${name} as ${origin}`);
