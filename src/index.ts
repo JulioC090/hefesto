95648a19a027a87a0e8ac22035b5e03eb1e83adc
JulioC090 <juliocziviani@gmail.com>
2024-08-11 17:51:59 -0300

    feat: add save command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program.command('save [name] [origin]').action((name, origin) => {
+  console.log(`Saving ${name} as ${origin}`);
+});
+
+program.parse(process.argv);
